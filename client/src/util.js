a81596dd22e230cc160d8e8a96d4bb5e7a92650e
Shashi Hans shashi.hans@coverstack.in <shashi.hans@coverstack.in>
2024-02-28 01:19:25 +0530

    api fixed

--- client/src/util.js
+++ client/src/util.js
@@ -1,4 +1,4 @@
 const baseURL = (window.location.hostname === "localhost") ?
           "http://localhost:4000" : "https://employee-app-oezp.onrender.com";
-const endpoint = '/api/employees';
-export const fullURL = `${baseURL}${endpoint}`;
+const end = '/api/employees';
+export const fullURL = `${baseURL}${end}`;
